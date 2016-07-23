36af901e0fecdc13d6e8584f7fe4ca95e1864ca7
monikkinom <monik@etiole.com>
2016-07-23 22:20:08 +0530

    admin changes

--- dreamadream/app/admin.py
+++ dreamadream/app/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 
-from models import Centre
+from models import *
 
 # Register your models here.
 
+admin.site.index_title = 'LiveADream'
+
 admin.site.register(Centre)
+admin.site.register(Student)
+admin.site.register(Mentor)
+admin.site.register(Workshop)
+admin.site.register(Meeting)
+admin.site.register(Program)
 
