dea9d32b5cd7ff26a14f2d58e8b746edfb0ad6b9
monik <monik@bitbucket.org>
2016-07-23 16:08:34 +0530

    centre model

--- dreamadream/app/models.py
+++ dreamadream/app/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Centre(models.Model):
+    name = models.CharField(max_length=100)
+    note = models.CharField(max_length=100)
+
+    class Meta:
+        app_label = 'app'
