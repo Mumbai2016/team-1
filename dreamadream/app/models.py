02afa50042de11facb6ab7d0af56bd493f7920a3
monik <monik@bitbucket.org>
2016-07-23 16:11:08 +0530

    migrations

--- dreamadream/app/models.py
+++ dreamadream/app/models.py
@@ -7,5 +7,8 @@ class Centre(models.Model):
     name = models.CharField(max_length=100)
     note = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         app_label = 'app'
